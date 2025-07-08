40141267b0b0e7b7c3decc08c3fa6e23fededda8
atharvadani9 <atharvadani9@gmail.com>
2025-07-08 22:06:59 +0530

    buttons are there but not aligned. WIP

--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -20,9 +20,89 @@ const Calculator = (): JSX.Element => {
 
   // calculate(payload);
 
+  const buttons = [
+    ["C", "+/-", "%", "/"],
+    ["7", "8", "9", "*"],
+    ["4", "5", "6", "-"],
+    ["1", "2", "3", "+"],
+    ["0", ".", "="],
+  ];
+
   return (
-    <Grid sx={{ display: "flex", justifyContent: "center" }}>
-      <Typography variant="h2">{"Calculator App"}</Typography>
+    <Grid
+      container
+      justifyContent="center"
+      sx={{ alignItems: "center", px: 2 }}
+      direction={"column"}
+    >
+      <Grid
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          pb: 2,
+          borderRadius: 1,
+          py: 2,
+        }}
+      >
+        <Typography variant="h2">{"Calculator"}</Typography>
+      </Grid>
+      <Grid
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          backgroundColor: "#333333",
+          borderRadius: 1,
+          p: 2,
+          // border: "1px solid #333",
+        }}
+      >
+        <Grid
+          container
+          sx={{
+            backgroundColor: "#1a1a1a",
+            borderRadius: 1,
+            px: 4,
+            py: 2,
+            textAlign: "center",
+            direction: "row",
+          }}
+        >
+          <Typography>{"0"}</Typography>
+        </Grid>
+        {buttons.map((row, i) => (
+          <Grid
+            container
+            key={i}
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              pt: 2,
+            }}
+          >
+            {row.map((button, j) => (
+              <Grid
+                key={j}
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  backgroundColor: "#222222",
+                  borderRadius: 1,
+                  px: 4,
+                  py: 2,
+                  minWidth: "50px",
+                  cursor: "pointer",
+                  "&:hover": {
+                    backgroundColor: "#333333",
+                  },
+                }}
+              >
+                <Typography>{button}</Typography>
+              </Grid>
+            ))}
+          </Grid>
+        ))}
+      </Grid>
     </Grid>
   );
 };
